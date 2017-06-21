eaf3e94333e9beccd81f596ec43f09d2d168c0e2
Gordon RASS-KWASI <grass-kw@e2r1p7.42.fr>
2017-06-21 14:36:51 +0200

    Playground de test de depart

---
+++ Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import CoreMotion
+import PlaygroundSupport
+
+let container = UIView(frame: CGRect(x: 0, y: 0, width: 365, height: 667))
+
+container.backgroundColor = .white
+
+
+PlaygroundPage.current.liveView = container
